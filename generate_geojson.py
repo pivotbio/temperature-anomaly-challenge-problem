a804a0ed3d5b053ef2e2229fda4d23bda9fb6dda
Michael Moser <michaeldmoser@gmail.com>
2023-08-06 14:11:49 -0600

    Fix dateline boundary issue

--- generate_geojson.py
+++ generate_geojson.py
@@ -4,19 +4,37 @@ import pandas as pd
 from shapely.geometry import Polygon
 
 def build_dataframe_from_file(file_path):
-    year = os.path.splitext(os.path.basename(file_path))[0].split(".")[0]  # Extracting year from filename like "2001.tmax"
+    year = os.path.splitext(os.path.basename(file_path))[0].split(".")[0]
     
     df = pd.read_csv(file_path, delim_whitespace=True, names=['Month', 'Day', 'GridBoxID', 'Longitude', 'Latitude', 'TempAnomaly'])
     
     df['Year'] = int(year)
     
-    df['geometry'] = df.apply(lambda row: Polygon([(row['Longitude'], row['Latitude']),
-                                                   (row['Longitude'] + 3.75, row['Latitude']),
-                                                   (row['Longitude'] + 3.75, row['Latitude'] + 2.5),
-                                                   (row['Longitude'], row['Latitude'] + 2.5),
-                                                   (row['Longitude'], row['Latitude'])]), axis=1)
-    
+    def create_polygon(row):
+        ll_lon = row['Longitude']
+        ll_lat = row['Latitude']
+        ur_lon = row['Longitude'] + 3.75
+        ur_lat = row['Latitude'] + 2.5
+        
+        # If the coordinates overflow the standard lat/lon ranges, 
+        # then we split the polygons at the date line or poles.
+        if ur_lon > 180:
+            ur_lon -= 360
+        if ll_lat < -90:
+            ll_lat += 180
+            ur_lat += 180
+        if ur_lat > 90:
+            ur_lat -= 180
+        
+        return Polygon([(ll_lon, ll_lat), 
+                        (ur_lon, ll_lat),
+                        (ur_lon, ur_lat),
+                        (ll_lon, ur_lat),
+                        (ll_lon, ll_lat)])
+
+    df['geometry'] = df.apply(create_polygon, axis=1)
     return df
+
 
 def maybe_filter_by_day(frames, filter=None):
     if filter is None:
@@ -48,12 +66,11 @@ def convert_to_geojson(file_list, output_directory, day_filter=None):
     make_per_day_geojson_files(filtered_df, output_directory)
 
 if __name__ == "__main__":
-    output_directory = "geo/"
+    output_directory = "app/public/geo/"
 
     # only grab from 2010 and up. Otherwise we're dealing with far too much data for the purposes of this challenge.
     file_list = [os.path.join("grid", file) for file in os.listdir("grid") if file.endswith(".tmax") and int(file.split(".")[0]) >= 2010]
     maybe_make_directory(output_directory)
     
-
     convert_to_geojson(file_list, output_directory)
 

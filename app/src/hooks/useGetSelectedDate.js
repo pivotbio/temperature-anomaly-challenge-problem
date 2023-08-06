b1b6f84bade402c5366a305681575bb69aa8cc79
Michael Moser <michaeldmoser@gmail.com>
2023-08-06 17:51:37 -0600

    Use June 30, 2022 as the default

--- app/src/hooks/useGetSelectedDate.js
+++ app/src/hooks/useGetSelectedDate.js
@@ -3,5 +3,5 @@ import { useParams } from "react-router-dom";
 export default function useGetSelectedDate() {
   const { selectedDate } = useParams();
 
-  return selectedDate ?? "2022-06-20";
+  return selectedDate ?? "2022-06-30";
 }
